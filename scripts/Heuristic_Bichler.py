c73b5c66650cce872aec65377683f9ce798826b3
Matthias Bichler <matthias-bichler@gmx.at>
2025-10-16 15:08:24 +0200

    deleted proofvalidmoves and added logic to generate moves

--- scripts/Heuristic_Bichler.py
+++ scripts/Heuristic_Bichler.py
@@ -1,4 +1,4 @@
-
+import copy
 #declare start-state
 
 start_state = [
@@ -21,7 +21,7 @@ def print_start_state():
 # check if solveable
 invertation_counter = 0
 one_dimensional_list = []
-coordinates = []
+
 
 def checkIfSolveable():
     global invertation_counter
@@ -40,36 +40,32 @@ def checkIfSolveable():
 # Funktion um die Position für 0 zu ermitteln
 # wird später benötigt, um die möglichen Schitte zu ermitteln
 def findPositionOfZero(state):
-    global coordinates
     for row in range(len(state)): # gehe Zeilen durch
         for column in range(len(state[row])): # gehe Spalten durch
             if state[row][column] == 0: #prüfe ob beides 0 ist (mit Zugriffsoperator)
-                coordinates.append(row)
-                coordinates.append(column)
-                print("Coordinates: ", coordinates)
-                zero_row = coordinates[0]
-                zero_column = coordinates[1]
-                print("zero row: ", zero_row, " zero column: ", zero_column)
-                return zero_row, zero_column
+                return row, column
 
 #Moves
 def generateMoves(state):
-    findPositionOfZero(state)
-    zero_row, zero_column = findPositionOfZero(state)
-    move_up = [zero_row - 1, zero_column]
-    move_down = [zero_row + 1, zero_column]
-    move_right = [zero_row, zero_column + 1]
-    move_left = [zero_row, zero_column - 1]
-    valid_moves = [move_up, move_down, move_right, move_left]
-    return valid_moves
+    possibleStates = []
+    zero_row, zero_col = findPositionOfZero(state)
+    moves = [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, -1)
+    ]
+    for r , c in moves:
+        new_row = zero_row + r
+        new_col = zero_col + c
+        if 0 <= new_row <= 2 and 0 <= new_col <= 2:
+            new_state = copy.deepcopy(state)
+            new_state[zero_row][zero_col], new_state[new_row][new_col] = new_state[new_row][new_col], new_state[zero_row][zero_col]
+            possibleStates.append(new_state)
+    return possibleStates
 
-#TODO
-def proofValidMoves(state):
-    generateMoves(state)
-    valid_moves = generateMoves(state)
 
-    for move in valid_moves:
-        if valid_moves[move] < [2, 2] and valid_moves > [0, 0]:
+
 
 
 
